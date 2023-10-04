5ccce826b7d25ee31ccdd7d9a9df7957015114d2
Shubham Agarwal <shubham.agarwal480@gmail.com>
2023-10-04 11:45:58 +0530

    Added some new code

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 import random
 import string
 
+with open("book.txt",encoding="UTF-8") as f:
+    word_bag = f.read().split(" ")
+    random.shuffle(word_bag)
+
 app = FastAPI()
 
 @app.get("/")
@@ -11,3 +15,7 @@ async def root():
 @app.get("/posts")
 async def randomMsg():
     return {"post": "".join([random.choice(string.ascii_letters) for i in range(50)])}
+
+@app.get("/passage")
+async def cryptic_msg ():
+    return {"msg": " ".join([random.choice(word_bag) for i in range(200)])}
