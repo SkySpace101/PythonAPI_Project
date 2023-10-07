bbed2b72102034d363fec59158f6515d817388c7
Shubham Agarwal <shubham.agarwal480@gmail.com>
2023-10-07 12:40:30 +0530

    Added Schema and Created Post Paths

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 import random
 import string
+
+
+# Validation for the Post Requests (Creating a Schema using Pydantic)
+class Post(BaseModel):
+    title:str
+    content:str
+    published:bool = False
+    rating: Optional[int] = None
 
 with open("book.txt",encoding="UTF-8") as f:
     word_bag = f.read().split(" ")
@@ -19,3 +30,9 @@ async def randomMsg():
 @app.get("/passage")
 async def cryptic_msg ():
     return {"msg": " ".join([random.choice(word_bag) for i in range(200)])}
+
+@app.post("/create")
+async def post_created(payload: Post):
+    print(payload)
+    # return {"data": "This is the data"}
+    return {"title":payload.title, "Content": payload.content}
