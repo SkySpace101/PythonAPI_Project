f5ad4970c79d3e3c70363e166cc4148336652dc1
Shubham Agarwal <shubham.agarwal480@gmail.com>
2023-12-25 13:29:50 +0530

    upadting other routes with the ORM database procdeure.

--- app/main.py
+++ app/main.py
@@ -113,16 +113,19 @@ async def get_latest_post():
 
 @app.get("/posts/{id}")
 # async def get_post(id:int, response: Response):
-async def get_post(id:int):
+async def get_post(id:int, db: Session = Depends(get_db)):
     # post = find_post(id)
     # return {"data": f"The post you requested {id}"}
-    print(id)
+    # print(id)
     try:
-        cursor.execute("SELECT * FROM posts WHERE id = %s", str(id))
-        post = cursor.fetchone()
+        # cursor.execute("SELECT * FROM posts WHERE id = %s", str(id))
+        # post = cursor.fetchone()
         # if (post == None):
             # raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The post with id {id} was not found.")
         # print(post)
+        post = db.query(models.Post).filter(models.Post.id == id).first()
+        if (post == None):
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The post with id {id} was not found.")
     except Exception as error:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The post with id {id} was not found.")
     
@@ -137,26 +140,45 @@ async def get_post(id:int):
 # Deleting a post
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(id: int):
-    index = find_post_index(id)
-    if(index == None):
+async def delete_post(id: int, db: Session = Depends(get_db) ):
+    # index = find_post_index(id)
+    # if(index == None):
+    #     raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"The post with id: {id} was not found.")
+    # my_posts.pop(index)
+    # Response(status_code=status.HTTP_204_NO_CONTENT)
+    post = db.query(models.Post).filter(models.Post.id == id) # saving the query in a variable post
+    if post.first() == None:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"The post with id: {id} was not found.")
-    my_posts.pop(index)
-    Response(status_code=status.HTTP_204_NO_CONTENT)
+    
+    post.delete(synchronize_session=False)
+    db.commit()
 
 # Updating the Post
 
 @app.put("/posts/{id}", status_code=status.HTTP_202_ACCEPTED)
-async def update_post(id: int, post: Post):
+async def update_post(id: int, update_post: Post, db: Session = Depends(get_db)):
     # print(post)
-    index = find_post_index(id)
-    if(index == None):
+    # index = find_post_index(id)
+    # if(index == None):
+    #     raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"The post with id: {id} was not found.")
+
+    # post_dict = post.model_dump()
+    # my_posts[index] = post_dict
+
+    post = db.query(models.Post).filter(models.Post.id == id)
+
+    if post.first() == None:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"The post with id: {id} was not found.")
+    
+    # post.update({"title": "this is the new title", "content": "this is the new content"}, synchronize_session=False)
+    post.update(update_post.model_dump(), synchronize_session=False)
 
-    post_dict = post.model_dump()
-    my_posts[index] = post_dict
+    # commit all the changes
+    db.commit()
 
-    return {"message": "updated post Successfully"}
+
+    # return {"message": "updated post Successfully"}
+    return {"data": post.first()}
 
 # defining a get route to test the db ORM.
 @app.get("/sqlalchemy")
